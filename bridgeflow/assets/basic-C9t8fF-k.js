83271f78f7ff93bac1ff0931f8dee05e6cb31b72
LIONBABYCRYPTO <lionbabycrypto@users.noreply.github.com>
2026-06-16 09:04:06 +0800

    deploy: BridgeFlow update

--- bridgeflow/assets/basic-C9t8fF-k.js
+++ bridgeflow/assets/basic-C9t8fF-k.js
@@ -1,4 +1,4 @@
-import{At as e,wt as t}from"./index-CwzM28y9.js";import{i as n,l as r,n as i,o as a,t as o}from"./lit-Cbk7b9dC.js";import{a as s,i as c,n as l,o as u,r as d,t as f}from"./wui-text-CC59bLY-.js";import{A as p,C as m,E as h,F as g,O as _,S as v,T as y,_ as b,a as x,b as S,g as C,i as ee,j as te,k as w,n as T,p as ne,r as E,s as D,u as re,x as O,y as k}from"./ConstantsUtil-BFFKPO2n.js";import{n as ie,t as ae}from"./ConnectorUtil-BqoKkubf.js";var oe=r`
+import{At as e,wt as t}from"./index-u4MAhYfr.js";import{i as n,l as r,n as i,o as a,t as o}from"./lit-Cbk7b9dC.js";import{a as s,i as c,n as l,o as u,r as d,t as f}from"./wui-text-C3DfbDW0.js";import{A as p,C as m,E as h,F as g,O as _,S as v,T as y,_ as b,a as x,b as S,g as C,i as ee,j as te,k as w,n as T,p as ne,r as E,s as D,u as re,x as O,y as k}from"./ConstantsUtil-C3qTk6Yx.js";import{n as ie,t as ae}from"./ConnectorUtil-DSMbX6TG.js";var oe=r`
   :host {
     position: relative;
     background-color: var(--wui-color-gray-glass-002);
